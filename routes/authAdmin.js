5fb500e9f54edb27413284b21313c2d0dca2f667
Mochammad nabiel <mochnabiel77@gmail.com>
2024-05-03 17:28:10 +0700

    fix authorization in authAdmin

--- routes/authAdmin.js
+++ routes/authAdmin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { register, login, profile } = require("../controller/authAdmin");
 const { authMiddleware } = require("../middleware/auth");
 
-router.post("/register", authMiddleware(["superadmin"]), register);
+router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", authMiddleware(["admin", "superadmin"]), profile);
 
