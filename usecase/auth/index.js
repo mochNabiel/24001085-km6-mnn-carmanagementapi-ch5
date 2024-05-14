f0402b17c523263eac11729d18d4919581745cff
Mochammad nabiel <mochnabiel77@gmail.com>
2024-05-14 10:51:11 +0700

    add password & role in createUser googleLogin usecase

--- usecase/auth/index.js
+++ usecase/auth/index.js
@@ -69,9 +69,10 @@ exports.googleLogin = async (accessToken) => {
   // Create new user based on google data that get by access_token
   const user = await createUser({
     email: googleData?.email,
-    password: "default",
+    password: crypto.randomBytes(16).toString("hex"),
     name: googleData?.given_name,
     photo: googleData?.picture,
+    role: "admin",
   });
 
   // Delete object password from user
